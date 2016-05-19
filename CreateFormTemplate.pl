e9437d3ee2b24d3c55d53453a28471b2f16be4e3
Eric Tran <ertran@portfoliorecovery.com>
2016-05-19 16:13:01 -0700

    working for strings now!!!

--- CreateFormTemplate.pl
+++ CreateFormTemplate.pl
@@ -27,8 +27,13 @@ my $file = $ARGV[0] or die "Need to get CSV file on the command line\n";
 my $sum = 0;
 open(my $data, '<', $file) or die "Could not open '$file' $!\n";
 while (my $line = <$data>) {
+	if ($line =~ /FriendlyName/) {
+		next;
+	}
+
 	chomp $line;
 	ProcessLine($line);
+	print "\n\n\n\n";
 }
 
 # print "$sum\n";
@@ -36,58 +41,106 @@ while (my $line = <$data>) {
 sub ProcessLine {
 
 	my ($line) = @_;
-	print "$line \n";
+	# print "my line: $line \n";
 
 	if ($csv->parse($line)) {
 
 		my @fields = $csv->fields();
 
 
+		if ($#fields != 5) {
+			warn "There needs to be **six** fields, you have $#fields"
+		}
+
+
+		my $friendlyName="";
+		my $codeName="";
+		my $type="";
+		my $span="";
+		my $maxLength="";
+		my $ngmax="";
+		my $max="";
+		my $required="";
+
 		for (my $i=0; $i <= $#fields; $i++) {
 
 			my $val = $fields[$i];
 			$val =~ s/^\s+|\s+$//g;
-			
 
 			# Let do the work and set the values... 
 			# FriendlyName	CodeName	Type	Span(9 highest & default)	MaxLength(default 50)	Required
 
-			my $friendlyName="";
-			my $codeName="";
-			my $type="";
-			my $span="";
-			my $maxlength="";
-			my $required="";
-
-
 			if ($i == 0) {
 				$friendlyName = $val;
 			}
-			if ($i == 0) {
+			if ($i == 1) {
 				$codeName = $val;
 			}
-			if ($i == 0) {
+			if ($i == 2) {
 				$type = $val;
 			}
-			if ($i == 0) {
+			if ($i == 3) {
 				$span = $val;
 			}
-			if ($i == 0) {
-				$maxlength = $val;
+			if ($i == 4) {
+				$maxLength = $val;
 			}
-			if ($i == 0) {
+			if ($i == 5) {
 				$required = $val;
 			}
+		}
+
+		# Now you can take these values and print what you need to print
+		# Here we will print different template based on the types
+
+		if ($span eq "") {
+			$span = "9";
+		}
+		if ($maxLength eq "") {
+			$ngmax = "ng_maxLength";
+			$max = "{{maxLength}}";
+		} else {
+			$ngmax = $maxLength;
+			$max = $maxLength
+		}
 
 
+		if ($required eq "") {
+			$required = "false";	
+		}
 
 
-			print $friendlyName."\n";
-            
+		if ($type =~ /^string$/i) {
+
+			# handle option fields
 
 
+			print '<div class="form-group" show-errors>'."\n";
+			print '<label for="'.$codeName.'" class="col-sm-3 control-label">'.$friendlyName.'</label>'."\n";
+			print '<div class="col-sm-'.$span.'">'."\n";
+			print '<input type="text" ng-maxlength="'.$ngmax.'" maxlength='.$max.' id="'.$codeName.'" name="'.$codeName.'" class="form-control"'."\n";
+			print 'ng-model="fmodel.'.$codeName.'" ng-required="'.$required.'" />'."\n";
+			print '<div class="help-block" ng-messages="myForm.'.$codeName.'.$error" ng-show="myForm.'.$codeName.'.$touched || submitted">'."\n";
+			print '<div ng-messages-include="app/html/error-messages.html"></div>'."\n";
+			print '</div>'."\n";
+			print '</div>'."\n";
+			print '</div>'."\n";
 
 		}
+
+		if ($type =~ /^number$/i) {
+			print "string hit --> $friendlyName\n";
+		}
+
+		if ($type =~ /^date$/i) {
+			print "string hit --> $friendlyName\n";
+		}
+
+		if ($type =~ /^checkbox$/i) {
+			print "string hit --> $friendlyName\n";
+		}
+
+
 
 	} else {
 		warn "Line could not be parsed: $line\n";
